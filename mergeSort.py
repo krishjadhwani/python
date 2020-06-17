b17979c771059d3a157dff2985f279de2a8436fe
Krish <eatatjoes0@gmail.com>
2020-06-16 23:43:51 -0500

    Updated input for mergeSort

--- mergeSort.py
+++ mergeSort.py
@@ -1,12 +1,5 @@
 import sys
 
-def getArrays(fileName):
-    file = open(fileName)
-    lines = []
-    for line in file:
-        lines.append(line.split(','))
-    return lines
-
 def mergeSortHelper(array, start, end):
     # Base Case
     if start == end:
@@ -17,13 +10,14 @@ def mergeSortHelper(array, start, end):
     mergeSortHelper(array, start, mid)
     mergeSortHelper(array, mid + 1, end)
 
-    # Merge
     start2 = mid + 1
 
     # Return early if largest element in first half
     # is smaller than smallest element in second half
     if (array[mid] < array[start2]):
         return
+
+    # Merge
     while (start <= mid and start2 <= end):
         if (array[start] < array[start2]):
             start +=1
@@ -37,16 +31,14 @@ def mergeSortHelper(array, start, end):
             start += 1
             mid += 1
             start2 += 1
-    print(array)
 
 def mergeSort(array):
     mergeSortHelper(array, 0, len(array) - 1)
 
 if __name__ == '__main__':
-    #fileName = sys.argv[1]
-    #arrays = getArrays(fileName)
-    arrays = [[
-        12,33,4,21,155,56,4,9
-    ]]
-    for array in arrays:
-        mergeSort(array)
+    n = int(input("Enter number of elements: "))
+    array = [] 
+    for i in range(n):
+        array.append(int(input("#{}:".format(i+1))))
+    mergeSort(array)
+    print(array)
